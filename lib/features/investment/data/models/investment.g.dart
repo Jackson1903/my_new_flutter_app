38ec24ede4a3b9baaa5354b32207c253c51de380
Jackson1903 <randallcerdasjackson@gmail.com>
2025-07-14 10:12:52 -0600

    Se atienden comentarios (#4)

--- lib/features/investment/data/models/investment.g.dart
+++ lib/features/investment/data/models/investment.g.dart
@@ -8,16 +8,16 @@ part of 'investment.dart';
 
 _$InvestmentImpl _$$InvestmentImplFromJson(Map<String, dynamic> json) =>
     _$InvestmentImpl(
-      cardId: json['cardId'] as String,
-      cardName: json['cardName'] as String,
-      cardType: json['cardType'] as String,
-      investmentAmount: json['investmentAmount'] as String,
+      cardId: json['id'] as String,
+      cardName: json['name'] as String,
+      cardType: json['type'] as String,
+      investmentAmount: json['amount'] as String,
     );
 
 Map<String, dynamic> _$$InvestmentImplToJson(_$InvestmentImpl instance) =>
     <String, dynamic>{
-      'cardId': instance.cardId,
-      'cardName': instance.cardName,
-      'cardType': instance.cardType,
-      'investmentAmount': instance.investmentAmount,
+      'id': instance.cardId,
+      'name': instance.cardName,
+      'type': instance.cardType,
+      'amount': instance.investmentAmount,
     };
